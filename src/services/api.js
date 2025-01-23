0fe681b7a5f8a47e7d0eec3fef67d0e6139aa0d9
Shripad SK <shripad.khandare@mitaoe.ac.in>
2025-01-24 00:28:07 +0530

    Updated API base URL for deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:9000/',
+  // baseURL: 'http://localhost:9000/',
+  baseURL: 'https://shareplate-backend-brv517daq-shripad-khandares-projects.vercel.app',
   withCredentials: true, // Include cookies
 });
 
